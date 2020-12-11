89bff5d15a70648b2a7798e2914e5f781736ed0d
Adrian Tomalla <adrian.tomalla@novatec-gmbh.de>
2020-12-11 19:41:20 +0100

    Implementing tcp listener on port 8080

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream){
+    let mut data = [0 as u8]; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    let  listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    println!("Server listening on port 8080");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+    Ok(())
 }
